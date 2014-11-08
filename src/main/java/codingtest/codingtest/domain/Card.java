9cfb65137adf6161519d9a928d6455b8069129d8
Dmitry <volchek.mail@gmail.com>
2014-11-08 20:37:47 +0300

    Updated domain

--- src/main/java/codingtest/codingtest/domain/Card.java
+++ src/main/java/codingtest/codingtest/domain/Card.java
@@ -3,5 +3,24 @@ package codingtest.codingtest.domain;
 /**
  * This is the domain class that represents a card in a card game.
  */
-public enum Card {
+public class Card {
+
+    private CardType type;
+    private CardSuit suit;
+
+    public CardType getType() {
+        return type;
+    }
+
+    public void setType(CardType type) {
+        this.type = type;
+    }
+
+    public CardSuit getSuit() {
+        return suit;
+    }
+
+    public void setSuit(CardSuit suit) {
+        this.suit = suit;
+    }
 }
--- src/main/java/codingtest/codingtest/domain/Deck.java
+++ src/main/java/codingtest/codingtest/domain/Deck.java
@@ -1,7 +1,19 @@
 package codingtest.codingtest.domain;
 
+import java.util.List;
+
 /**
  * This is the class that represents a deck of cards in a card game.
  */
 public class Deck {
+
+    private List<Card> cards;
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
+    }
 }

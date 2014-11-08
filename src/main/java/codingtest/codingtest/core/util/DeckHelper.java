669edb03a629997d2fff4a3935b5bee009419c02
Dmitry <volchek.mail@gmail.com>
2014-11-08 20:58:56 +0300

    Shuffle created

--- src/main/java/codingtest/codingtest/core/util/DeckHelper.java
+++ src/main/java/codingtest/codingtest/core/util/DeckHelper.java
@@ -1,13 +1,18 @@
 package codingtest.codingtest.core.util;
 
+import codingtest.codingtest.domain.Deck;
+
+import java.util.Collections;
+import java.util.List;
+
 public class DeckHelper {
 
 	public static void createDeck() {
 
 	}
 
-	public static void shuffleDeck() {
-
+	public static void shuffleDeck(List<Deck> deck) {
+        Collections.shuffle(deck);
 	}
 
 }
--- src/main/java/codingtest/codingtest/domain/enums/CardSuit.java
+++ src/main/java/codingtest/codingtest/domain/enums/CardSuit.java
@@ -1,6 +1,8 @@
 package codingtest.codingtest.domain.enums;
 
-
+/**
+ * This enum represents the suit of a card
+ */
 public enum CardSuit {
 
     HEARTS, DIAMONDS, CLUBS, SPADES;
